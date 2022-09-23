96d4d8faac5ebbdb7e88a6e80620de8f7c94169a
brickly26 <sabburak26@gmail.com>
2022-09-23 17:12:02 -0400

    completed assignment 2

--- Section4/app.js
+++ Section4/app.js
@@ -4,16 +4,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!")
-  next() // Allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+  console.log("This middleware always runs!!!!")
+  next()
 })
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
   console.log("In another middleware!")
-  // ..
-})
+  res.send("<h1>The 'Add Product Page'!</h1>")
+});
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  console.log("In another middleware!")
+  res.send("<h1>Hello from Express!</h1>")
+});
 
-server.listen(3001);
+app.listen(3001)
