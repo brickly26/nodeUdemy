ce61fa3447ee4eb97648c4c4713747c25a9782ed
brickly26 <sabburak26@gmail.com>
2022-09-23 18:27:47 -0400

    learned to use express routers

--- Section4/app.js
+++ Section4/app.js
@@ -1,22 +1,16 @@
 const http = require('http');
+const bodyParser = require('body-parser')
 
 const express = require('express');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
+
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log("This middleware always runs!!!!")
-  next()
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  console.log("In another middleware!")
-  res.send("<h1>The 'Add Product Page'!</h1>")
-});
-
-app.use('/', (req, res, next) => {
-  console.log("In another middleware!")
-  res.send("<h1>Hello from Express!</h1>")
-});
+app.use(adminRoutes)
+app.use(shopRoutes)
 
 app.listen(3001)
